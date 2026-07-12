7367a6a162bd1aec2086d33f3ef4af10a9ef3a41
yehoshua <y0587828030@gmail.com>
2026-07-12 11:46:35 +0300

    step 2 Customer Greeting

--- oop.py
+++ oop.py
@@ -13,3 +13,15 @@ print(m1.__dict__)
 m1.describe()
 
 
+## step 2
+class Customer:
+    def __init__(self, name, favorit_drink):
+        self.name = name
+        self.drink = favorit_drink
+
+    def greet(self):
+        print(f"Hi! I am {self.name} and I would like a {self.drink}.")
+
+coustimr1 =  Customer("alice","latte" )
+coustimr1.greet()
+        
